970376261a0ace2edb17e94d053863413e692d66
PlugaruT <tudor.plugaru@amdaris.com>
2020-07-14 09:32:59 +0300

    Implement unit of work

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -12,9 +12,13 @@ from allocation.adapters import repository
 
 
 class AbstractUnitOfWork(abc.ABC):
-    # should this class contain __enter__ and __exit__?
-    # or should the context manager and the UoW be separate?
-    # up to you!
+    @abc.abstractmethod
+    def __enter__(self, *args, **kwargs):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __exit__(self, *args, **kwargs):
+        raise NotImplementedError
 
     @abc.abstractmethod
     def commit(self):
@@ -31,13 +35,20 @@ DEFAULT_SESSION_FACTORY = sessionmaker(bind=create_engine(
 ))
 
 
-class SqlAlchemyUnitOfWork:
-    ...
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    
+    def __init__(self, session_factory=DEFAULT_SESSION_FACTORY) -> None:
+        self.session_factory = session_factory
 
-# One alternative would be to define a `start_uow` function,
-# or a UnitOfWorkStarter or UnitOfWorkManager that does the
-# job of context manager, leaving the UoW as a separate class
-# that's returned by the context manager's __enter__.
-#
-# A type like this could work?
-# AbstractUnitOfWorkStarter = ContextManager[AbstractUnitOfWork]
+    def __enter__(self, *args, **kwargs):
+        self.session = self.session_factory()
+        self.batches = repository.SqlAlchemyRepository(self.session)
+
+    def __exit__(self, *args, **kwargs):
+        self.session.close()
+
+    def commit(self):
+        self.session.commit()
+
+    def rollback(self):
+        self.session.rollback()
